a75cb57ad8a82e2d3e05aa6bb0abbd01469ba4bc
Sean Maxwell <katanablaydz@yahoo.com>
2019-09-06 08:01:20 -0500

    Added fail fast for configs

--- src/main/java/com/blog/crud/BlogCrudApplication.java
+++ src/main/java/com/blog/crud/BlogCrudApplication.java
@@ -2,8 +2,10 @@ package com.blog.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class BlogCrudApplication {
 
 	public static void main(String[] args) {
