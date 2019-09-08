3aca389a8065881eaa7133b24a184dbdba2c79c2
Sean Maxwell <katanablaydz@yahoo.com>
2019-09-08 17:49:04 -0500

    Modified startup script to wait for config server to finish starting

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
+echo "********************************************************"
+while ! `nc -z configserver $CONFIGSERVER_PORT `; do sleep 3; done
+echo ">>>>>>>>>>>> Configuration Server has started"
+
 echo "********************************************************"
 echo "Starting blog service "
 echo "********************************************************"
