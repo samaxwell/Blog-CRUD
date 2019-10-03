d001011bc0723c121c1801fd35ba03eb4cda0b89
Sean Maxwell <katanablaydz@yahoo.com>
2019-10-02 19:12:18 -0500

    Added basic logging to controller

--- src/main/java/com/blog/crud/controllers/BlogController.java
+++ src/main/java/com/blog/crud/controllers/BlogController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.blog.crud.domain.BlogDto;
 import com.blog.crud.services.BlogService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/v1/blogs")
 public class BlogController {
@@ -24,16 +27,19 @@ public class BlogController {
 	
 	@GetMapping
 	public List<BlogDto> getAllBlog() {
+		log.info("Received request for all blogs");
 		return blogService.getAllBlogs();
 	}
 	
 	@GetMapping("/{id}")
 	public BlogDto getBlogById(@PathVariable Long id) {
+		log.info("Received request for blog {}", id);
 		return blogService.findById(id);
 	}
 	
 	@PostMapping
 	public BlogDto createBlog(@Valid @RequestBody BlogDto blog) {
+		log.info("Received request to update blog {}", blog.getId());
 		return blogService.saveBlog(blog);
 	}
 
